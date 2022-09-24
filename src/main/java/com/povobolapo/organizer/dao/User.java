353ff1cfda173167c28b1dc0f299a20392477a0e
vlolad <vlad19.99@yandex.ru>
2022-09-24 15:07:25 +0300

    Add User model

--- src/main/java/com/povobolapo/organizer/dao/User.java
+++ src/main/java/com/povobolapo/organizer/dao/User.java
@@ -7,8 +7,76 @@ import javax.persistence.*;
 public class User {
 
     @Id
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "login")
+    private String login;
 
+    @Column(name = "password")
+    private String password;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    public User() {
+    }
+
+    public User(Integer id, String login, String password, String name, String avatar) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public User(String login, String password, String name, String avatar) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 }
