96a3501be9e691fc578469bb61306efaa76ea030
vlolad <vlad19.99@yandex.ru>
2022-09-28 23:12:53 +0300

    add user controller

--- src/main/java/com/povobolapo/organizer/controller/UserController.java
+++ src/main/java/com/povobolapo/organizer/controller/UserController.java
@@ -1,8 +1,36 @@
 package com.povobolapo.organizer.controller;
 
-import org.springframework.stereotype.Controller;
+import com.povobolapo.organizer.model.UserEntity;
+import com.povobolapo.organizer.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.xml.bind.ValidationException;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public UserEntity getUserByLogin
+            (@RequestParam String login) throws ValidationException {
+        log.debug("GET-request: getUserByLogin (login={})", login);
+        if (login == null) {
+            log.error("Login is NULL.");
+            throw new ValidationException("Login cannot be NULL.");
+        }
+        return userService.getUserByLogin(login);
+    }
+
 
 }
