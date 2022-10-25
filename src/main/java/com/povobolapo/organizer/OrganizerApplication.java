c561024aaf63b3e944522f04537bd222a63fe5d9
PavelProjects <paveloalexeev52@gmail.com>
2022-10-25 18:01:43 +0300

    changed packaging from war to jar; removed datasource config class

--- src/main/java/com/povobolapo/organizer/OrganizerApplication.java
+++ src/main/java/com/povobolapo/organizer/OrganizerApplication.java
@@ -2,11 +2,9 @@ package com.povobolapo.organizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.povobolapo.organizer")
-public class OrganizerApplication  extends SpringBootServletInitializer {
-
+public class OrganizerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizerApplication.class, args);
 	}
