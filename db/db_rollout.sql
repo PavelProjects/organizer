bc45959eafc6f19ccec5a6202156cf0cfd777c99
PavelProjects <paveloalexeev52@gmail.com>
2022-11-13 22:30:31 +0300

    добавлен нормальный docker compose файл для поднятия сервера

--- db/db_rollout.sql
+++ db/db_rollout.sql
@@ -1,8 +1,8 @@
 begin;
 
-drop table _user, _user_credits, _comment, _notification, _task, _user_task, dict_notify_type, dict_task_status;
-drop sequence main_id_sequence;
-drop function getnextid;
+--drop table _user, _user_credits, _comment, _notification, _task, _user_task, dict_notify_type, dict_task_status;
+--drop sequence main_id_sequence;
+--drop function getnextid;
 
 create sequence main_id_sequence;
 
